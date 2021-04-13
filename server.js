0e00e3cafb2ac3f2c2e27b6e79f20db9e83c11df
Glitch (therapeutic-ten-walker) <none>
2021-04-13 02:42:50 +0000

    Router params

--- server.js
+++ server.js
@@ -4,39 +4,55 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set("view engine", "pug");
 app.set("views", "./views");
-// https://expressjs.com/en/starter/basic-routing.html
-var todos = [
-  { id: 1, work: "Đi học" },
-  { id: 2, work: "Nấu cơm" },
-  { id: 3, work: "Rửa bát" },
-  { id: 4, work: "Học tại coderX" }
-];
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({ todos: [] }).write();
+
+const shortid = require('shortid');
+
+
+
 app.get("/", (req, res) => {
   res.render("index", {
-    todos: todos
+    todos: db.get("todos").value()
   });
 });
-app.get("/todos", (req, res) => {
-  var q = req.query.q;
-  var matchedTodos = todos.filter(function(todo) {
-    return todo.work.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
+app.get("/todos", (req, res)=>{
+  var q= req.query.q;
+  var todos= db.get("todos").value();
+  var matchedTodos = todos.filter(x=>x.work.toLowerCase().indexOf(q.toLowerCase())!==-1)
   res.render("index", {
     todos: matchedTodos
-  });
-});
+  })
+})
 app.post("/todos/create", (req, res) => {
-  todos.push(req.body);
-  res.redirect("/")
+  req.body.id = shortid.generate();
+  db.get("todos").push(req.body).write();
+  res.redirect("/");
 });
+app.get("/todos/:id/delete", (req, res)=>{
+  var id = req.params.id;
+  db.get("todos").remove({id:id}).write();
+  res.redirect("/")
+  
+  
+})
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
 });
+
+
+// Set some defaults (required if your JSON file is empty)
+
